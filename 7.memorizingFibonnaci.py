2ce5a837fae15e70f766c1133821525c25e5ecde
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-12-10 01:18:39 -1200

    Create 7.memorizingFibonnaci.py

---
+++ 7.memorizingFibonnaci.py
@@ -0,0 +1,22 @@
+#memorization fibonnaci sequence
+#cache decorator ---> funtools
+#0 1 1 2 3 5 8
+#fn = (fn - 1) + (fn - 2)
+def memorize(fibo):
+    cache = {1:1,2:1}
+    def inner(n):
+        if n not in cache:
+            cache[n] = fibo(n)
+        return cache[n]
+    return inner
+
+@memorize
+def fibo(n):
+    print('Calculating fibo{0}'.format(n))
+    return 1 if n<3 else fibo(n-1) + fibo(n-2)
+
+print(fibo(6))
+print('After printing fibo 6')
+print(fibo(7))
+print('After printing fibo 7')
+print(fibo(10))
