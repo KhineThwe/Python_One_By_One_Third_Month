a3635c4ab4447f14573192844f9efbf1a3fa779c
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-12-13 00:12:58 -1200

    Create 21classBuiltinattr.py

---
+++ 21classBuiltinattr.py
@@ -0,0 +1,11 @@
+#class built in attr
+#class တစ်ခုမှာ built in attr 5 ku shi
+#1.__dict__ အချက်လက်အားလုံးဖော်ပြ
+#2.__doc__ class တစ်ခုရဲ့မှတ်ချက်ကိုပြန်ပြ
+#3.__name__ class ရဲ့ name ကိုရလိုလျှင်
+#4.__module__
+#5.bases
+class MyClass:
+    def __init__(self):
+        pass
+print(MyClass.__dict__)#dic return pyan pay
